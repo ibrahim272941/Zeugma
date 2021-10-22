f4493b53ddf0f842906d2f021ffb28881110e165
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2021-10-23 01:15:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const content1 = document.getElementsByClassName('content1')[0];
+
+content1.addEventListener('mouseover',()=>{
+    content1.style.backgroundImage =' url(IMG/8.jpg)'
+    content1.style.transition='1.5s'
+    
+    
+})
+content1.addEventListener('mouseout',()=>{
+    content1.style.backgroundImage =' url(IMG/2.jpg)'
+    
+})
+
+const content2 = document.getElementsByClassName('content2')[0];
+content2.addEventListener('mouseover',()=>{
+    content2.style.backgroundImage =' url(IMG/7.jpg)'
+    content2.style.transition='1.5s'
+    
+    
+})
+content2.addEventListener('mouseout',()=>{
+    content2.style.backgroundImage =' url(IMG/5.jpg)'
+    content2.style.transition='1.5s'
+})
+
+
+const content12 = document.getElementsByClassName('content12')[0];
+content12.addEventListener('mouseover',()=>{
+    content12.style.backgroundImage =' url(IMG/11.jpg)'
+    content12.style.transition='1.5s'
+    
+    
+})
+content12.addEventListener('mouseout',()=>{
+    content12.style.backgroundImage =' url(IMG/9.jpg)'
+    content12.style.transition='1.5s'
+})
+
+
+
+const content8 = document.getElementsByClassName('content8')[0];
+content8.addEventListener('mouseover',()=>{
+    content8.style.backgroundImage =' url(IMG/Zeugma_painting.jpg)'
+    content8.style.transition='1.5s'
+    
+    
+})
+content8.addEventListener('mouseout',()=>{
+    content8.style.backgroundImage =' url(IMG/10.jpg)'
+    content8.style.transition='1.5s'
+})
+
+
+const content10 = document.getElementsByClassName('content10')[0];
+content10.addEventListener('mouseover',()=>{
+    content10.style.backgroundImage =' url(IMG/17.jpg)'
+    content10.style.transition='1.5s'
+    
+    
+})
+content10.addEventListener('mouseout',()=>{
+    content10.style.backgroundImage =' url(IMG/6.jpg)'
+    content10.style.transition='1.5s'
+})
